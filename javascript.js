cf42c306fc61f4092a155d590e1e9addec6d62cb
Umair Ahsan <umairahsan208@gmail.com>
2025-05-13 20:13:31 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// This one is for all pages
+console.log("Hello World");
+
+// This one is for when a contact form gets submitted
+function handleSubmit() {
+    console.log("Form submitted successfully!");
+    return false;
+  }
